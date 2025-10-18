98ceb40bc8c2a442673988a8320b6ad9af62a592
Albena Veseli <albena.veseli@student.uni-pr.edu>
2025-10-18 15:36:28 +0200

    vrejtja e gestureEnabled!

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <>
       <StatusBar style="dark" />
-      <Stack screenOptions={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false, gestureEnabled: false }} />
     </>
   );
 }
